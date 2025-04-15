68ef72e736112a93c6ab3580bfddaa51ba94a0ba
mooninbeom <dlsqja567@naver.com>
2025-04-15 20:16:19 +0900

    Initial Commit

---
+++ Run Mile/Run_MileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Run_MileApp.swift
+//  Run Mile
+//
+//  Created by 문인범 on 4/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Run_MileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Run MileTests/Run_MileTests.swift
@@ -0,0 +1,17 @@
+//
+//  Run_MileTests.swift
+//  Run MileTests
+//
+//  Created by 문인범 on 4/15/25.
+//
+
+import Testing
+@testable import Run_Mile
+
+struct Run_MileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
