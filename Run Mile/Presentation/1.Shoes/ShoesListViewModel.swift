de35dcbbab36d2faff0d38d8c5fa08e119370471
mooninbeom <dlsqja567@naver.com>
2025-04-19 14:43:09 +0900

    [#7] 신발 리스트 뷰 데이터 연결

--- Run Mile/Presentation/1.Shoes/ShoesListViewModel.swift
+++ Run Mile/Presentation/1.Shoes/ShoesListViewModel.swift
@@ -10,7 +10,14 @@ import Foundation
 
 @Observable
 final class ShoesListViewModel {
+    public var shoes: [Shoes] = []
     
+    
+    let useCase: ShoesListUseCase
+    
+    init(useCase: ShoesListUseCase) {
+        self.useCase = useCase
+    }
 }
 
 
@@ -18,5 +25,16 @@ extension ShoesListViewModel {
     @MainActor
     public func addShoesButtonTapped() {
         NavigationCoordinator.shared.push(.addShoes)
+    }
+    
+    public func onAppear() {
+        Task {
+            do {
+                let result = try await self.useCase.fetchShoes()
+                self.shoes = result
+            } catch {
+                
+            }
+        }
     }
 }
