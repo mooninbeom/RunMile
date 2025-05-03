19e9ff40ab0b0699f4b6af7f30fd7dc7865ce14b
mooninbeom <dlsqja567@naver.com>
2025-05-03 15:54:43 +0900

    [#13] HOF 플레이스홀더 구현

--- Run Mile/Presentation/3.MyPage/3-1.HallOfFame/HOFView.swift
+++ Run Mile/Presentation/3.MyPage/3-1.HallOfFame/HOFView.swift
@@ -10,7 +10,19 @@ import SwiftUI
 struct HOFView: View {
     var body: some View {
         VStack {
-            Text("Hello World!")
+            HStack {
+                Text("마일리지 목표를 달성한 신발을 보관합니다!")
+                    .font(FontStyle.workoutSubtitle())
+                
+                // TODO: 셀 구현
+                
+                Spacer()
+            }
+            
+            Spacer()
         }
+        .navigationTitle("명예의 전당")
+        .navigationBarTitleDisplayMode(.large)
+        .padding(.horizontal, 20)
     }
 }
