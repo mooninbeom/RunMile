defc65f07bf2aca9987f6040e8b4c7936925aff5
mooninbeom <dlsqja567@naver.com>
2025-06-09 00:01:27 +0900

    [#23] 건강 데이터 허락 없을 시 fitnessConnect뷰로 네비게이션 연결 구현

--- Run Mile/Presentation/2.Workout/WorkoutListViewModel.swift
+++ Run Mile/Presentation/2.Workout/WorkoutListViewModel.swift
@@ -58,7 +58,7 @@ extension WorkoutListViewModel {
     
     @MainActor
     public func workoutNotVisibleButtonTapped() {
-//        NavigationCoordinator.shared.switchAndPush(.myPage, tab: .myPage)
+        NavigationCoordinator.shared.switchAndPush(.fitnessConnect, tab: .myPage)
     }
     
     @MainActor
